a1caee319819e45a40454c9f49d5f09fe99506ef
salledevage <alextrington@gmail.com>
2022-08-20 11:27:31 -0400

    commit automatique

--- src/monitor/settings.py
+++ src/monitor/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os, dotenv
 from telnetlib import SE
-from tkinter import Scale
 
 dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
