c810503f276347f614c2a928644fa6bb755b9a25
salledevage <alextrington@gmail.com>
2022-08-21 07:37:06 -0400

    commit automatique

--- src/monitor/settings.py
+++ src/monitor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-DJANGO_ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS")
+DJANGO_ALLOWED_HOSTS = list(os.getenv("DJANGO_ALLOWED_HOSTS"))
 print(DJANGO_ALLOWED_HOSTS)
 ALLOWED_HOSTS = DJANGO_ALLOWED_HOSTS
 
