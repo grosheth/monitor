eaf9407a8fa87368f1ed51a31f883a5bfeb83099
salledevage <alextrington@gmail.com>
2022-08-23 17:10:40 -0400

    commit automatique

--- src/monitor/urls.py
+++ src/monitor/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
+from controller.views import dashboard
 
 urlpatterns = [
+    path('', dashboard),
     path('admin/', admin.site.urls),
 ]
