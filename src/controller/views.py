6cdda7e7fdb7fb96323951fc7f08361454e8fc95
salledevage <alextrington@gmail.com>
2022-08-23 17:18:35 -0400

    commit automatique

--- src/controller/views.py
+++ src/controller/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def dashboard(request):
-    return HttpResponse("<h1>Dashboard pour Kubernetes</h1>")
+    return render(request, "controller/dashboard.html")
