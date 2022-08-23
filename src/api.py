88abeaf9f5129070ef6593f1328cd4498e1830f6
salledevage <alextrington@gmail.com>
2022-08-23 08:39:09 -0400

    commit automatique

--- src/api.py
+++ src/api.py
@@ -4,26 +4,27 @@ import dotenv, os
 
 # Faire une connection ssh sur le PI et lancer les commandes via ce shell
 
-dotenv.load_dotenv()
-
-client = SSHClient()
-
-client.load_host_keys("ssh/known_hosts")
-client.load_system_host_keys()
-
-client.set_missing_host_key_policy(AutoAddPolicy())
-
-
-client.connect(os.getenv("IP_ADDRESS"), username=os.getenv("SSH_USER"))
+dotenv.load_dotenv("monitor/.env")
 def connect():
-    return
+    client = SSHClient()
+
+    client.load_host_keys("ssh/known_hosts")
+    client.load_system_host_keys()
+
+    client.set_missing_host_key_policy(AutoAddPolicy())
+
+    client.connect(os.getenv("IP_ADDRESS"), username=os.getenv("SSH_USER"))
+    return client
 
 
-def getInfo():
-    info = subprocess.run("kubectl describe pods")
-    info = info.stdout
-    return
+def command(command):
+    client = connect(client)
+    stdout= client.exec_command(command)
+    output = f'{stdout.read().decode("utf8")}'
+    stdout.close
+    client.close
+    return output
 
 
 if __name__ == '__main__':
-    connect()
+    command("kubectl get pods")
