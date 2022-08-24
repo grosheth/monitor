ca3ac0acec89682a6577542adf01d73ea4d42eae
salledevage <alextrington@gmail.com>
2022-08-23 22:18:04 -0400

    commit automatique

--- src/controller/views.py
+++ src/controller/views.py
@@ -7,9 +7,6 @@ def dashboard(request):
 
 def pods(request):
     namelist = api.command("kubectl get pods --output name")
-    print(namelist)
-    print(type(namelist))
+    print(list(namelist))
     number_of_pods = api.command("kubectl get pods --output name | wc -l")
-    print(number_of_pods)
-    print(type(number_of_pods))
-    return render(request, "controller/pods.html", context={"length": number_of_pods, "namelist": namelist})
+    return render(request, "controller/pods.html", context={"length": int(number_of_pods), "namelist": list(namelist)})
