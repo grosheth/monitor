789843abc6870bc21af433d729fca856d0639163
salledevage <alextrington@gmail.com>
2022-08-23 21:37:51 -0400

    commit automatique

--- src/controller/views.py
+++ src/controller/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
+import controller
 
 # Create your views here.
 def dashboard(request):
     return render(request, "controller/dashboard.html", context={"test": range(5)})
 
 def pods(request):
-    return render(request, "controller/pods.html", context={"test": range(5)})
+    namelist = list(controller.command("kubectl get pods --output name"))
+    number_of_pods = controller.command("kubectl get pods --output name | wc -l")
+    return render(request, "controller/pods.html", context={"length": number_of_pods, "namelist": namelist})
