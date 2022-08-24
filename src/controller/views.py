c36e6bbd675f76a276b4a1abae8d69c896c391ba
salledevage <alextrington@gmail.com>
2022-08-23 20:58:03 -0400

    commit automatique

--- src/controller/views.py
+++ src/controller/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "controller/dashboard.html", context={"test": 5})
+    return render(request, "controller/dashboard.html", context={"test": range(5)})
