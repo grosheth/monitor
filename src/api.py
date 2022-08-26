51e2358fdc5e2c0636bec45aec4a5df8dc6ef18d
salledevage <alextrington@gmail.com>
2022-08-26 07:37:07 -0400

    commit automatique

--- src/api.py
+++ src/api.py
@@ -5,7 +5,7 @@ import dotenv, os
 
 def connect():
 
-    dotenv.load_dotenv("monitor/.env")
+    dotenv.load_dotenv()
     SSH_USER = str(os.getenv("SSH_USER"))
     IP = str(os.getenv("IP_ADDRESS"))
     client = SSHClient()
